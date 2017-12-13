92e3791e8fc0302434d71490c2395c3e6d062393
jencekade <gaephuoc@gmail.com>
2017-12-13 07:19:13 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+const playlist = {};
+
+function updatePlaylist(playlist, artist, song) {
+  return Object.assign({},playlist, { artist:song});
+}
+
+function removeFromPlaylist(playlist, artist) {
+
+}
